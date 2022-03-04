4085c20ca7b6d5566ffe35aabf9200c35145cc94
Tcodrz <ttomz4291@gmail.com>
2022-03-04 12:40:40 +0200

    material init

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+  ],
+  exports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+  ]
+})
+export class MaterialModule { }
