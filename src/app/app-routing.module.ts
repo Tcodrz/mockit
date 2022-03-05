232f930ed9829387a62d4b1e5872f2160d0992fe
Tcodrz <ttomz4291@gmail.com>
2022-03-05 16:46:00 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'schemas', loadChildren: () => import('./schemas/schemas.module').then(m => m.SchemasModule) },
+  { path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -9,11 +10,13 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [
     MatToolbarModule,
     MatIconModule,
+    MatButtonModule,
   ],
   exports: [
     CommonModule,
     MatToolbarModule,
     MatIconModule,
+    MatButtonModule,
   ]
 })
 export class MaterialModule { }
